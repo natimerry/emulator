e2d95380f445623e7e72f302dadaa4b56b33af4a
Soham Nandy <soham.nandy2006@gmail.com>
2026-06-10 21:00:13 +0530

    richer logging

--- src/windows-emulator/syscalls/process.cpp
+++ src/windows-emulator/syscalls/process.cpp
@@ -13,6 +13,8 @@ namespace sogen
                                                   const uint64_t process_information, const uint32_t process_information_length,
                                                   const emulator_object<uint32_t> return_length)
         {
+            c.win_emu.log.print(color::dark_gray, "--> Process info class: 0x%X length=%u\n", info_class, process_information_length);
+
             if (!c.proc.is_current_process_handle(process_handle))
             {
                 return STATUS_NOT_SUPPORTED;
@@ -251,6 +253,9 @@ namespace sogen
         NTSTATUS handle_NtSetInformationProcess(const syscall_context& c, const handle process_handle, const uint32_t info_class,
                                                 const uint64_t process_information, const uint32_t process_information_length)
         {
+            c.win_emu.log.print(color::dark_gray, "--> Set process info class: 0x%X length=%u\n", info_class,
+                                process_information_length);
+
             if (!c.proc.is_current_process_handle(process_handle))
             {
                 return STATUS_NOT_SUPPORTED;
